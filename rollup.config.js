a93a84f074be752f1b9c1a65ee38fd9f653629f2
Luis Lobo <luis.lobo9b@gmail.com>
2024-11-07 04:20:28 -0300

    criado arquivo rollup.config.js com import do plugin-node-resolve para importar da pasta node_modules e plugin-replace para substituir valores como process.env.NODE_ENV

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import { nodeResolve } from "@rollup/plugin-node-resolve"
+import replace from "@rollup/plugin-replace"
+
+export default {
+	input: "src/js/app.mjs",
+	output: {
+		file: "src/js/app.build.mjs",
+		format: "esm"
+	},
+	plugins: [
+		nodeResolve(),
+		replace({
+			"process.env.NODE_ENV": JSON.stringify("production"),
+			preventAssignment: true
+		})
+	]
+}
